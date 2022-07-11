d7dd2b80df75466d3c98369a0877bd16c9784f7c
Sid-2000 <108202405+Sid-2000@users.noreply.github.com>
2022-07-11 11:05:09 +0530

    Add files via upload

---
+++ Apple.java
@@ -0,0 +1,17 @@
+
+
+
+public class Apple implements Fruit {
+
+	@Override
+	final public void cut() {
+		System.out.println("cutting apple...");
+	}
+
+	@Override
+	public void makeJuice() {
+
+		System.out.println("making apple juice... ");
+	}
+
+}
